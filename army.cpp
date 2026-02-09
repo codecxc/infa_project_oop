ee8e6ce77c12fe09c078d243cf5824a471475113
IRON-DOME <popovzazert@gmail.com>
2026-02-09 18:21:45 +0300

    ничего полезного

---
+++ army.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+
+
+class Army {
+	protected:
+	 
+	bool  muzzle; //наличие дула или ствола для выстрела
+	bool warhead; //наличие трюма для выпуска боеголовки
+	bool gun;     // наличие автоматического оружия(пулемёт)
+	bool tactical_defence; //наличие тактической защиты
+	
+	int quan_muzzle;  // патроны ствола
+	int quan_warhead; // патроны боеголовок
+	int quan_gun; 	  // патроны пулемёта
+	int quan_defence; // заряд тактической защиты
+	
+	private:
+
+	
+
+
+
+
+};
+
+
+
